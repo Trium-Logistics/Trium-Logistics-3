a457089542c1a1d2004b56aea6cd5969a5a64d27
Marcelo Uchôas <56437644+marcelouchoas@users.noreply.github.com>
2024-05-08 00:16:04 -0300

    Add files via upload

---
+++ otimizacao.py
@@ -0,0 +1,188 @@
+from pulp import *
+
+problema1 = LpProblem('mizimize_Custos',LpMinimize)
+
+x15 = LpVariable('x15', lowBound = 0)
+x18 = LpVariable('x18', lowBound = 0)
+x19 = LpVariable('x19', lowBound = 0)
+x110 = LpVariable('x110', lowBound = 0)
+x111 = LpVariable('x111', lowBound = 0 )
+x124 = LpVariable('x124', lowBound = 0)
+x125 = LpVariable('x125', lowBound = 0)
+x126 = LpVariable('x126', lowBound = 0)
+x127 = LpVariable('x127', lowBound = 0)
+x128 = LpVariable('x128', lowBound = 0)
+x129 = LpVariable('x129', lowBound = 0)
+x130 = LpVariable('x130', lowBound = 0)
+x131 = LpVariable('x131', lowBound = 0)
+x132 = LpVariable('x132', lowBound = 0)
+x133 = LpVariable('x133', lowBound = 0)
+x134 = LpVariable('x132', lowBound = 0)
+x135 = LpVariable('x135', lowBound = 0)
+x136 = LpVariable('x136', lowBound = 0)
+x137 = LpVariable('x137', lowBound = 0)
+x138 = LpVariable('x138', lowBound = 0)
+x139 = LpVariable('x139', lowBound = 0)
+x140 = LpVariable('x140', lowBound = 0)
+x141 = LpVariable('x140', lowBound = 0)
+x142 = LpVariable('x140', lowBound = 0)
+x143 = LpVariable('x143', lowBound = 0)
+x147 = LpVariable('x147', lowBound = 0)
+x148 = LpVariable('x148', lowBound = 0)
+x149 = LpVariable('x149', lowBound = 0)
+x150 = LpVariable('x150', lowBound = 0)
+x151 = LpVariable('x151', lowBound = 0)
+x21 = LpVariable('x21', lowBound = 0)
+x22 = LpVariable('x22', lowBound = 0)
+x23 = LpVariable('x23', lowBound = 0)
+x24 = LpVariable('x24', lowBound = 0)
+x25 = LpVariable('x25', lowBound = 0)
+x26 = LpVariable('x26', lowBound = 0)
+x27 = LpVariable('x27', lowBound = 0)
+x28 = LpVariable('x28', lowBound = 0)
+x29 = LpVariable('x29', lowBound = 0)
+x210 = LpVariable('x210', lowBound = 0)
+x211 = LpVariable('x211', lowBound = 0)
+x212 = LpVariable('x212', lowBound = 0)
+x213 = LpVariable('x213', lowBound = 0)
+x214 = LpVariable('x214', lowBound = 0)
+x215 = LpVariable('x215', lowBound = 0)
+x216 = LpVariable('x216', lowBound = 0)
+x217 = LpVariable('x217', lowBound = 0)
+x218 = LpVariable('x218', lowBound = 0)
+x219 = LpVariable('x219', lowBound = 0)
+x220 = LpVariable('x220', lowBound = 0)
+x221 = LpVariable('x221', lowBound = 0)
+x222 = LpVariable('x222', lowBound = 0)
+x223 = LpVariable('x223', lowBound = 0)
+x224 = LpVariable('x224', lowBound = 0)
+x225 = LpVariable('x225', lowBound = 0)
+x226 = LpVariable('x226', lowBound = 0)
+x227 = LpVariable('x227', lowBound = 0)
+x228 = LpVariable('x228', lowBound = 0)
+x229 = LpVariable('x229', lowBound = 0)
+x230 = LpVariable('x230', lowBound = 0)
+x231 = LpVariable('x231', lowBound = 0)
+x232 = LpVariable('x232', lowBound = 0)
+x233 = LpVariable('x233', lowBound = 0)
+x234 = LpVariable('x234', lowBound = 0)
+x235 = LpVariable('x235', lowBound = 0)
+x239 = LpVariable('x239', lowBound = 0)
+x240 = LpVariable('x240', lowBound = 0)
+x241 = LpVariable('x241', lowBound = 0)
+x242 = LpVariable('x242', lowBound = 0)
+x243 = LpVariable('x243', lowBound = 0)
+x244 = LpVariable('x244', lowBound = 0)
+x245 = LpVariable('x245', lowBound = 0)
+x246 = LpVariable('x246', lowBound = 0)
+x247 = LpVariable('x247', lowBound = 0)
+x248 = LpVariable('x248', lowBound = 0)
+x249 = LpVariable('x249', lowBound = 0)
+x250 = LpVariable('x250', lowBound = 0)
+x251 = LpVariable('x251', lowBound = 0)
+x31 = LpVariable('x31', lowBound = 0)
+x32 = LpVariable('x32', lowBound = 0)
+x33 = LpVariable('x33', lowBound = 0)
+x34 = LpVariable('x34', lowBound = 0)
+x35 = LpVariable('x35', lowBound = 0)
+x36 = LpVariable('x36', lowBound = 0)
+x37 = LpVariable('x27', lowBound = 0)
+x38 = LpVariable('x38', lowBound = 0)
+x39 = LpVariable('x39', lowBound = 0)
+x310 = LpVariable('x310', lowBound = 0)
+x311 = LpVariable('x311', lowBound = 0)
+x319 = LpVariable('x319', lowBound = 0)
+x320 = LpVariable('x320', lowBound = 0)
+x321 = LpVariable('x321', lowBound = 0)
+x322 = LpVariable('x322', lowBound = 0)
+x323 = LpVariable('x323', lowBound = 0)
+x324 = LpVariable('x324', lowBound = 0)
+x325 = LpVariable('x325', lowBound = 0)
+x326 = LpVariable('x326', lowBound = 0)
+x327 = LpVariable('x327', lowBound = 0)
+x328 = LpVariable('x328', lowBound = 0)
+x329 = LpVariable('x329', lowBound = 0)
+x330 = LpVariable('x330', lowBound = 0)
+x331 = LpVariable('x331', lowBound = 0)
+x332 = LpVariable('x332', lowBound = 0)
+x333 = LpVariable('x333', lowBound = 0)
+x334 = LpVariable('x334', lowBound = 0)
+x335 = LpVariable('x335', lowBound = 0)
+x336 = LpVariable('x336', lowBound = 0)
+x337 = LpVariable('x337', lowBound = 0)
+x338 = LpVariable('x338', lowBound = 0)
+x344 = LpVariable('x344', lowBound = 0)
+x345 = LpVariable('x345', lowBound = 0)
+x346 = LpVariable('x346', lowBound = 0)
+x347 = LpVariable('x347', lowBound = 0)
+x348 = LpVariable('x328', lowBound = 0)
+x349 = LpVariable('x329', lowBound = 0)
+x350 = LpVariable('x350', lowBound = 0)
+x351 = LpVariable('x351', lowBound = 0)
+
+
+problema1 += 4.02*x15+3.96*x18+3.78*x19+3.55*x110+3.95*x111+3.71*x124+4.05*x125+3.47*x126+3.91*x127+3.41*x128+3.24*x129+2.46*x130+3.58*x131+2.96*x132+1.67*x133+3.80*x134+3.85*x135+2.20*x136+2.28*x137+2.29*x138+1.90*x139+1.69*x140+1.83*x141+1.84*x142+1.69*x143+1.51*x147+1.43*x148+1.74*x149+1.52*x150+1.62*x151+1.80*x21+2.18*x22+2.37*x23+2.28*x24+3.12*x25+1.90*x26+1.96*x27+1.91*x28+2.42*x29+1.44*x210+2.78*x211+1.30*x212+1.36*x213+1.46*x214+1.24*x215+1.23*x216+1.24*x217+1.25*x218+1.48*x219+1.37*x220+1.30*x221+1.48*x222+1.34*x223+1.46*x224+1.65*x225+1.48*x226+1.46*x227+1.42*x228+1.32*x229+1.31*x230+1.48*x231+1.50*x232+1.57*x233+1.51*x234+1.49*x235+1.17*x239+1.08*x240+1.14*x241+1.15*x242+1.06*x243+1.63*x244+1.62*x245+1.61*x246+3.66*x247+3.69*x248+3.49*x249+2.96*x250+2.50*x251+2.51*x31+2.81*x32+2.76*x33+2.60*x34+2.60*x35+2.78*x36+2.44*x37+2.82*x38+2.22*x39+3.64*x310+1.88*x311+3.59*x319+3.60*x320+3.64*x321+3.56*x322+4.05*x323+3.57*x324+3.74*x325+3.69*x326+5.05*x327+3.37*x328+2.89*x329+3.44*x330+3.75*x331+3.73*x332+3.73*x333+3.60*x334+3.58*x335+1.41*x336+1.47*x337+1.48*x338+2.22*x344+2.18*x345+2.21*x346+1.17*x347+1.10*x348+0.95*x349+1.02*x350+0.92*x351
+
+
+
+problema1 += x15 + x18 + x19 + x110 + x111 + x124 + x125 + x126 + x127 + x128 + x129 + x130 + x131 + x132 + x133 + x134 + x135 + x136 + x137 + x138 + x139 + x140 + x141 + x141 + x142 + x143 + x147 + x148 + x149 + x150 + x151 <= 90000000
+problema1 += x21 + x22 + x23 + x24 + x25 + x26 + x27 + x28 + x29 + x210 + x211 + x212 + x213 + x214 + x215 + x216 + x217 + x218 + x219 + x220 + x221 + x222 + x223 + x224 + x225 + x226 + x227 + x228 + x229 + x230 + x231 + x232 + x233 + x234 + x235 + x239 + x24 + x241 + x242 + x243 + x244 + x245 + x246 + x247 + x248 + x249 + x250 + x251 <= 56000000
+problema1 += x31 + x32 + x33 + x34 + x35 + x36 + x37 + x38 + x39 + x310 + x311 + x319 + x320 + x321 + x322 + x323 + x324 + x325 + x326 + x327 + x328 + x329 + x330 + x331 + x332 + x333 + x334 + x335 + x336 + x337 + x338 + x344 + x345 + x346 + x347 + x348 + x34 + x350 + x351 <= 90000000
+problema1 += x21 + x31 == 5973721
+problema1 += x22 + x32 == 1778080
+problema1 += x23 + x33 == 5958798
+problema1 += x24 + x34 == 896173
+problema1 += x15 + x25 + x35 == 3241494
+problema1 += x26 + x36 == 3244827
+problema1 += x27 + x37 == 12738726
+problema1 += x18 + x28 + x38 == 6792503
+problema1 += x19 + x29 + x39 == 7471374
+problema1 += x110 + x210 + x310 == 2098730
+problema1 += x111 + x211 + x311 == 7295028
+problema1 += x212 == 1350774
+problema1 += x213 == 1439856
+problema1 += x214 == 3977784
+problema1 += x215 == 3666906
+problema1 += x216 == 271034
+problema1 += x217 == 1272373
+problema1 += x218 ==  569236
+problema1 += x219 + x319 == 1589336
+problema1 += x220 + x320 == 5063433
+problema1 += x221 + x321 == 10686204
+problema1 += x222 + x322 == 2495205
+problema1 += x223 + x323 == 1753764
+problema1 += x124 + x224 + x324 == 20427048
+problema1 += x125 + x225 + x325 == 7828763
+problema1 += x126 + x226 + x326 == 5788209
+problema1 += x127 + x227 + x327 == 11836544
+problema1 += x128 + x228 + x328 == 6145143
+problema1 += x129 + x229 + x329 == 13860432
+problema1 += x130 + x230 + x330 == 3482379
+problema1 += x131 + x231 + x331 == 4084642
+problema1 += x132 + x232 + x332 == 10839219
+problema1 += x133 + x233 + x333 == 1336988
+problema1 += x134 + x234 + x334 == 1898750
+problema1 += x135 + x335 == 14197671
+problema1 += x136 + x336 == 1716192
+problema1 += x137 + x337 == 827342
+problema1 += x138 + x338 == 2539443
+problema1 += x139 + x239 == 1789064
+problema1 += x140 + x240 == 973767
+problema1 += x141 + x241 == 5304924
+problema1 += x142 + x242 == 838856
+problema1 += x143 + x243 == 8150094
+problema1 += x244 + x344 == 678417
+problema1 += x245 + x345 == 3600095
+problema1 += x246 + x346 == 3522678
+problema1 += x147 + x247 + x347 == 3675315
+problema1 += x148 + x248 + x348 == 1310374
+problema1 += x149 + x249 + x349 == 1761137
+problema1 += x150 + x250 + x350 == 4402893
+problema1 += x151 + x251 + x351 == 1331761
+
+problema1.solve()
+
+for v in problema1.variables():
+    print(v.name, "=", v.varValue)
+
+print('Resultado do custo mínimo =', value(problema1.objective))
